620168a00ce53dddb60b01185c075540a96141a4
Ankit Gyanchand <ankit.gyanchand@quietplatforms.com>
2024-12-12 23:08:41 +0530

    added addition

--- ProductOfNumers.py
+++ ProductOfNumers.py
@@ -3,7 +3,10 @@
 def get_product(num_1, num_2):
     return num_1*num_2
 
+def get_sum(num_1, num_2):
+    return num_1+num_2
 
 
 if __name__=="__main__":
     print(f'The product of the numbers is: {get_product(5, 7)}')
+    print(f'The addition of the numbers is: {get_sum(5, 7)}')
